a22f8fbcbd605c0494af8143d659ab5892b4bd73
willo8 <willos18@hotmail.co.uk>
2019-04-08 19:37:54 +0100

    Add Music Notes

---
+++ HelloWorld/Music.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class Music
+    {
+        public string note { get; set; }
+        public int position { get; set; }
+        public bool enabled { get; set; }
+    }
+}
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -17,22 +17,71 @@ namespace HelloWorld
             string name = Console.ReadLine();
             Console.WriteLine("Hello " + name);
             Console.ReadKey();
-            
-            Console.WriteLine("How Old Are You?");
-            if (int.TryParse(Console.ReadLine(), out age)){
-                if (age >= 18)
+
+           
+            bool hasitworked = false;
+            while (!hasitworked)
+            {
+                Console.WriteLine("How Old Are You?");
+                if (int.TryParse(Console.ReadLine(), out int age))
                 {
-                    Console.WriteLine("Access Granted");
+                    if (age >= 18)
+                    {
+                        Console.WriteLine("Access Granted");
+                        mynewfunction();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Access Denied");
+                        my2ndfunction();
+                    }
+                    hasitworked = true;
                 }
                 else
                 {
-                    Console.WriteLine("Access Denied");
+                    Console.WriteLine("I do not understand characters");
                 }
             }
             Console.WriteLine();
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+        }
+
+        private static void mynewfunction()
+        {
+            Console.WriteLine("This is inside my new function");
+            my2ndfunction();
+        }
+
+        private static void my2ndfunction()
+        {
+            Console.WriteLine("This is inside my 2nd function");
+            List<Music> musiclist = new List<Music>();
+            Music firstnote = new Music();
+            firstnote.note = "C";
+            firstnote.enabled = true;
+            firstnote.position = 1;
+
+            Music secondnote = new Music();
+            secondnote.note = "C#";
+            secondnote.enabled = false;
+            secondnote.position = 2;
+
+            Music thirdnote = new Music();
+            thirdnote.note = "D";
+            thirdnote.enabled = true;
+            thirdnote.position = 3;
+
+            musiclist.Add(firstnote);
+            musiclist.Add(secondnote);
+            musiclist.Add(thirdnote);
+
+            foreach (var note in musiclist.Where(note => note.enabled))
+            {
+                Console.Write(note.note);
+                Console.Write(",");
+            }
         }
     }
 }
